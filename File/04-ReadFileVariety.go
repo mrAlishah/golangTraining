023e5f2111fecc1632e36237748086fb77503427
mrAlishah <mrAlishPrg@gmail.com>
2023-02-25 20:19:24 +0330

    Fil-04-Variety of file reading

---
+++ File/04-ReadFileVariety.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+const fileName = "01.txt"
+
+func main() {
+	ReadFile(fileName)
+	ReadFileByBuffer(fileName)
+	ReadFileByStringsBuilder(fileName)
+	ReadFileByEmbedPkg()
+} 
+
+func ReadFile(filename string){
+	fmt.Println("\nread file by os.ReadFile ------------------------------")
+    f, err := os.ReadFile(filename)
+	if err != nil{
+		log.Fatal(err)
+	  }
+	//defer f.Close()
+
+	//contents := f.String()
+	contents := string(f)
+
+    fmt.Print(contents)
+}
+
+func ReadFileByBuffer(filename string) {
+	fmt.Println("\nread file by bytes.Buffer------------------------------")	
+	f, err := os.Open(filename)
+	if err != nil{
+	  log.Fatal(err)
+	}
+	defer f.Close()
+ 
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(f)
+	contents := buf.String()
+ 
+	fmt.Print(contents) 
+}		
+
+func ReadFileByStringsBuilder(filename string) {
+	fmt.Println("\nread file by strings.Builder------------------------------")	
+	f, err := os.Open(filename)
+	if err != nil{
+	  log.Fatal(err)
+	}
+	defer f.Close()
+ 
+	sb := new(strings.Builder)
+	io.Copy(sb, f)
+	contents := sb.String()
+ 
+	fmt.Print(contents) 
+}		
+
+//go:embed 01.txt
+var contents string
+//embed cannot apply to var inside func
+func ReadFileByEmbedPkg() {
+	fmt.Println("\nread file by embed PKG------------------------------")	
+ 
+	fmt.Print(contents) // print the content as a 'string'
+}		
