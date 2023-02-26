cd4292c335c31b2cda7c13589b5fd7a31d30c74f
mrAlishah <mrAlishPrg@gmail.com>
2023-02-26 16:44:47 +0330

    File-03,04-Write comment

--- File/03-ioutilPKG.go
+++ File/03-ioutilPKG.go
@@ -6,51 +6,52 @@ import (
 	"os"
 )
 
+// ioutil.WriteFile and ioutil.ReadFile is deprecated for go > 1.16
 func main() {
 
 	//! Go 1.16 or later :: Replace ioutil with os
-    mydata := []byte("Appending some text to file\n")
+	mydata := []byte("Appending some text to file\n")
 
 	//------------- Write to file
-    // the WriteFile method returns an error if unsuccessful
+	// the WriteFile method returns an error if unsuccessful
 	//* Delete 03.txt file if exist and re-create it
-    fmt.Println("\nStart to write text file ------------------------------")	
-    err := ioutil.WriteFile("03.txt", mydata, 0777)
-    // handle this error
-    if err != nil {
-        // print it out
-        fmt.Println(err)
-    }
+	fmt.Println("\nStart to write text file ------------------------------")
+	err := ioutil.WriteFile("03.txt", mydata, 0777)
+	// handle this error
+	if err != nil {
+		// print it out
+		fmt.Println(err)
+	}
 
-	//------------- Read from file	
-	fmt.Println("\nStart to read text file ------------------------------")	
-    data, err := ioutil.ReadFile("03.txt")
-    if err != nil {
-        fmt.Println(err)
-    }
+	//------------- Read from file
+	fmt.Println("\nStart to read text file ------------------------------")
+	data, err := ioutil.ReadFile("03.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    fmt.Print(string(data))
+	fmt.Print(string(data))
 
 	//------------- Write to file
-    // the WriteFile method returns an error if unsuccessful
-    fmt.Println("\nStart to write text file ------------------------------")	
-    f, err := os.OpenFile("03.txt", os.O_APPEND|os.O_WRONLY, 0600)
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
+	// the WriteFile method returns an error if unsuccessful
+	fmt.Println("\nStart to write text file ------------------------------")
+	f, err := os.OpenFile("03.txt", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-    if _, err = f.WriteString("new Appending some text to file\n"); err != nil {
-        panic(err)
-    }
-	
-	//------------- Read from file	
-	fmt.Println("\nStart to read text file ------------------------------")	
-    data, err = ioutil.ReadFile("03.txt")
-    if err != nil {
-        fmt.Println(err)
-    }
+	if _, err = f.WriteString("new Appending some text to file\n"); err != nil {
+		panic(err)
+	}
 
-    fmt.Print(string(data))
+	//------------- Read from file
+	fmt.Println("\nStart to read text file ------------------------------")
+	data, err = ioutil.ReadFile("03.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Print(string(data))
 
 }
--- File/04-ReadFileVariety.go
+++ File/04-ReadFileVariety.go
@@ -17,57 +17,58 @@ func main() {
 	ReadFileByBuffer(fileName)
 	ReadFileByStringsBuilder(fileName)
 	ReadFileByEmbedPkg()
-} 
+}
 
-func ReadFile(filename string){
+func ReadFile(filename string) {
 	fmt.Println("\nread file by os.ReadFile ------------------------------")
-    f, err := os.ReadFile(filename)
-	if err != nil{
+	f, err := os.ReadFile(filename)
+	if err != nil {
 		log.Fatal(err)
-	  }
-	//defer f.Close()
+	}
+	//defer f.Close() //don't need
 
-	//contents := f.String()
-	contents := string(f)
+	//contents := f.String() //cast is different
+	contents := string(f) //[]byte
 
-    fmt.Print(contents)
+	fmt.Print(contents)
 }
 
 func ReadFileByBuffer(filename string) {
-	fmt.Println("\nread file by bytes.Buffer------------------------------")	
+	fmt.Println("\nread file by bytes.Buffer------------------------------")
 	f, err := os.Open(filename)
-	if err != nil{
-	  log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
 	}
 	defer f.Close()
- 
+
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(f)
-	contents := buf.String()
- 
-	fmt.Print(contents) 
-}		
+	contents := buf.String() //*bytes.Buffer
+
+	fmt.Print(contents)
+}
 
 func ReadFileByStringsBuilder(filename string) {
-	fmt.Println("\nread file by strings.Builder------------------------------")	
+	fmt.Println("\nread file by strings.Builder------------------------------")
 	f, err := os.Open(filename)
-	if err != nil{
-	  log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
 	}
 	defer f.Close()
- 
+
 	sb := new(strings.Builder)
 	io.Copy(sb, f)
-	contents := sb.String()
- 
-	fmt.Print(contents) 
-}		
+	contents := sb.String() //*strings.Builder
+
+	fmt.Print(contents)
+}
 
 //go:embed 01.txt
 var contents string
-//embed cannot apply to var inside func
+
+// embed cannot apply to var inside func
 func ReadFileByEmbedPkg() {
-	fmt.Println("\nread file by embed PKG------------------------------")	
- 
+	fmt.Println("\nread file by embed PKG------------------------------")
+
 	fmt.Print(contents) // print the content as a 'string'
-}		
+}
