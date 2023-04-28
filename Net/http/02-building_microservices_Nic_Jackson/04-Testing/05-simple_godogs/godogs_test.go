7e23ed298f97ec9e376ed5301e8f8639be109cdf
mrAlishah <mrAlishPrg@gmail.com>
2023-04-28 17:36:47 +0330

    Net-02-04-05-Simple BDD and Godog: Cucumber for Golang

---
+++ Net/http/02-building_microservices_Nic_Jackson/04-Testing/05-simple_godogs/godogs_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/cucumber/godog"
+)
+
+// godogsCtxKey is the key used to store the available 05-simple_godogs in the context.Context.
+type godogsCtxKey struct{}
+
+func thereAreGodogs(ctx context.Context, available int) (context.Context, error) {
+	return context.WithValue(ctx, godogsCtxKey{}, available), nil
+}
+
+func iEat(ctx context.Context, num int) (context.Context, error) {
+	available, ok := ctx.Value(godogsCtxKey{}).(int)
+	if !ok {
+		return ctx, errors.New("there are no 05-simple_godogs available")
+	}
+
+	if available < num {
+		return ctx, fmt.Errorf("you cannot eat %d 05-simple_godogs, there are %d available", num, available)
+	}
+
+	available -= num
+
+	return context.WithValue(ctx, godogsCtxKey{}, available), nil
+}
+
+func thereShouldBeRemaining(ctx context.Context, remaining int) error {
+	available, ok := ctx.Value(godogsCtxKey{}).(int)
+	if !ok {
+		return errors.New("there are no 05-simple_godogs available")
+	}
+
+	if available != remaining {
+		return fmt.Errorf("expected %d 05-simple_godogs to be remaining, but there is %d", remaining, available)
+	}
+
+	return nil
+}
+
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"features"},
+			TestingT: t, // Testing instance that will run subtests.
+		},
+	}
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
+}
+
+func InitializeScenario(sc *godog.ScenarioContext) {
+	sc.Step(`^there are (\d+) 05-simple_godogs$`, thereAreGodogs)
+	sc.Step(`^I eat (\d+)$`, iEat)
+	sc.Step(`^there should be (\d+) remaining$`, thereShouldBeRemaining)
+}
