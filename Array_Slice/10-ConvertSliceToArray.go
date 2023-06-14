935a0669e10d646530dde74aa95644eed2e9774e
mrAlishah <mrAlishPrg@gmail.com>
2023-06-14 13:01:51 +0330

    Slice-06-zero slice & append variadic into slice

--- Array_Slice/10-ConvertSliceToArray.go
+++ Array_Slice/10-ConvertSliceToArray.go
@@ -16,6 +16,7 @@ func main() {
 	*/
 
 	fmt.Printf("%T : %+v\n", s, s)
+	fmt.Printf("%T : %+v\n", a, a)
 
 }
 
@@ -23,4 +24,5 @@ func main() {
 []int : [1 2]
 *[2]int : &[1 2]
 []int : [1 2 3]
+*[2]int : &[1 2]
 */
